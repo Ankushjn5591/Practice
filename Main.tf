8596bbb689340c4e3d19d47952fe6fe884d23b1c
Ankush Jain <ankushjn38@gmaial.com>
2023-05-09 00:27:51 +0530

    added rg1

--- Main.tf
+++ Main.tf
@@ -7,14 +7,19 @@ resource "azurerm_resource_group" "rg" {
   location = "East US"
 }
 
+data "azurerm_resource_group" "rg1" {
+  name         = "keyvaultrg"
+}
 data "azurerm_key_vault" "key" {
   name         = "firstkeyvault5591"
+  resource_group_name = data.azurerm_resource_group.rg1.name
 }
 
 
 data "azurerm_key_vault_secret" "storage_access_key" {
   name         = "storagekey"
   key_vault_id = data.azurerm_key_vault.key.id
+  resource_group_name = data.azurerm_resource_group.rg1.name
 }
 
 
