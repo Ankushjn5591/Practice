73f7809fbf94322f598bef25385e4805dec0ef96
Ankush Jain <ankushjn38@gmaial.com>
2023-05-08 22:33:41 +0530

    backend config

--- Main.tf
+++ Main.tf
@@ -4,6 +4,15 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "rg" {
   name = "ankushrg"
-  location = "West Europe"
+  location = "East US"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "Storagerg"
+    storage_account_name = "storageaccount5591"
+    container_name       = "tfstate"
+    key                  = "devpipeline.terraform.tfstate"
+    access_key           = "9DcT8nW/iKr0v2t8bfFIfM24sfJRGva1oD4macMbw6UkSwUXYHJr0ErQzgv15oErzQebT6lpi4zl+ASt2Lfeeg=="
+  }
+}
