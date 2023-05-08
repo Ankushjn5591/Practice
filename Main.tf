66e3010274ba079df3cd579c259a7d0ad67ad88c
Ankush Jain <ankushjn38@gmaial.com>
2023-05-08 23:42:11 +0530

    value

--- Main.tf
+++ Main.tf
@@ -16,12 +16,16 @@ data "azurerm_key_vault_secret" "keysecret" {
   key_vault_id = azurerm_key_vault.key.id
 }
 
+output "example_secret_value" {
+  value = data.azurerm_key_vault_secret.example.value
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "Storagerg"
     storage_account_name = "storageaccount5591"
     container_name       = "tfstate"
     key                  = "devpipeline.terraform.tfstate"
-    access_key           = data.azurerm_key_vault_secret.keysecret.value
+    access_key           = "${output.exmaple_secret_value.value}"
   }
 }
