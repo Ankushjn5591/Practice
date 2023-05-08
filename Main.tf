7e8c049045f6fc4b1eae90a11460ef97bc5ccaad
Ankush Jain <ankushjn38@gmaial.com>
2023-05-08 15:04:45 +0530

    added Static

--- Main.tf
+++ Main.tf
@@ -60,7 +60,7 @@ resource "azurerm_public_ip" "pip" {
   name                = "PIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "static"
+  allocation_method   = "Static"
 }
 resource "azurerm_network_interface" "nic" {
   name                = "vm1nic"
