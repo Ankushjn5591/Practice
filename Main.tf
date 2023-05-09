b8feacffa344ce333a47fb8483ea598016a262cc
Ankush Jain <ankushjn38@gmaial.com>
2023-05-09 15:40:01 +0530

    changed cidr address

--- Main.tf
+++ Main.tf
@@ -9,14 +9,14 @@ resource "azurerm_resource_group" "rg" {
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.vnetname}"
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["192.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
    name                = "${var.subname}"
-  address_prefixes     = ["10.0.0.0/24"]
+  address_prefixes     = ["192.0.0.0/24"]
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
 }
