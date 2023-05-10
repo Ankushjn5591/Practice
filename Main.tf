673404f3839e148b89ec5af1feb22648d0d1e299
Ankush Jain <ankushjn38@gmaial.com>
2023-05-10 14:10:06 +0530

    added sec rule

--- Main.tf
+++ Main.tf
@@ -26,6 +26,19 @@ resource "azurerm_network_security_group" "nsg" {
   location            = "${azurerm_resource_group.rg.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
 
+  
+   security_rule {
+    name                       = "Allow-Internet"
+    priority                   = 290
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
   security_rule {
     name                       = "Allow-RDP"
     priority                   = 300
